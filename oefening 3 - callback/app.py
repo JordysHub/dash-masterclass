57b2e62b7a1c3b77af38478c3678fa3bd3af983c
jordy_schep <jordy.schep88@gmail.com>
2022-10-24 15:55:04 +0200

    oef 3

---
+++ oefening 3 - callback/app.py
@@ -0,0 +1,79 @@
+from dash import Dash, dcc, html, Input, Output
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+
+
+df = pd.read_csv("./data/night_out.csv")
+
+layout_dict = {
+    'backgroundColor': 'lightgreen',
+    'header': 'darkblue',
+    'text': 'darkred',
+    'textAlign': 'center'
+    }
+
+
+
+# TODO 1: DROPDOWN
+
+DROPDOWN_CONTENT = dcc.Dropdown()
+
+
+
+# TODO 2: DROPDOWN
+
+GRAPH_CONTENT = dcc.Graph()
+
+
+# TODO 3: CALLBACKS
+
+INPUT_CONTENT = Input()
+OUTPUT_CONTENT = Output()
+
+
+# TODO 4: UPDATE_FUNCTION
+
+
+
+app = Dash(__name__)
+
+app.layout = html.Div(
+    children=[
+        html.Div(
+            html.H1("Hello, Best Talent Group Ever!"), style={"textAlign": "center", "color": "darkblue"}
+        ),
+
+        html.P(
+            children="Welcome to this Masterclass",
+            style={"textAlign": layout_dict["textAlign"], "color": layout_dict["text"]},
+        ),
+
+
+        # TODO 1
+        DROPDOWN_CONTENT,
+
+        # TODO 2
+        GRAPH_CONTENT
+    ]
+)
+
+
+@app.callback(
+    # TODO 3
+    OUTPUT_CONTENT,
+    INPUT_CONTENT
+)
+
+def update_output_fig(input_value):
+
+    # TODO 4
+
+    fig = px.bar(new_df, x="City", y="Cost", color="Category")
+
+    return fig
+
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
